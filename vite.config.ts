c1b1c9f71bf23f8049ae776aa1561a90f25e5d6e
Miny <944307891@qq.com>
2022-05-13 22:13:05 +0800

    chore: 配置api代理

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,20 @@ import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import OPENHUB_API from './src/service/api'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  server: {
+    proxy: {
+      '/api': {
+        target: OPENHUB_API,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   plugins: [
     vue(),
     AutoImport({
