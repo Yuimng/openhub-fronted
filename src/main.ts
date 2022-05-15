14f2be3a47e77e7bd07ed7000a9a7e82f4dcfe0a
Miny <944307891@qq.com>
2022-05-15 19:48:11 +0800

    feat(登录): router验证token存在免登录

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import App from './App.vue'
 import './assets/css/index.less'
 import router from './router'
 import { setupStore } from './store'
+import { initAppConfigStore } from './logics/initAppConfig'
 
 const app = createApp(App)
 
 setupStore(app)
+
+initAppConfigStore()
 
 app.use(router).mount('#app')
